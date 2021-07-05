65d552fbd55526dc2bc7cd2cf1fc04b61c2f43b0
Miguel Sama <miguelsamamerino@gmail.com>
2021-07-06 00:59:58 +0200

    actions updated

--- pkg/bot/actions.go
+++ pkg/bot/actions.go
@@ -2,6 +2,7 @@ package bot
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/mikeletux/EMT-Go-Telegram-Bot/pkg/emt"
 )
@@ -34,14 +35,34 @@ func (b botActionsMap) PerformAction(command string, args []string) (string, err
 		return b.PrintAllHelp(), nil
 	}
 	// Check if action exists
-	v, ok := b[command]
+	action, ok := b[command]
 	if !ok {
 		return "", fmt.Errorf("command not found")
 	}
+
+	// Check argument length is ok
+	if len(args) != len(action.Arguments) {
+		return "", fmt.Errorf("number of arguments incorrect. Please check help")
+	}
+
+	// TO-DO
+
+	return "", nil
 }
 
 func (b botActionsMap) PrintAllHelp() string {
 	var help string
+	help += "Help from EMT Telegram Bot\n---\n"
+
+	keys := make([]string, 0, len(b))
+	for k := range b {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		help += printHelp(b[k]) + "\n---"
+	}
 
 	return help
 }
